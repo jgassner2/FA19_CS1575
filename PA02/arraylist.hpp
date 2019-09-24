161b5e89c3c9e81bc75b2c2f81689cfb7e6b9640
Jonathan Gassner <jmgkc7@mst.edu>
2019-09-24 11:32:56 -0500

    Added Destructor, size, and clear functions.  So far they are working with empty lists.  Will check again with data.

--- PA02/arraylist.hpp
+++ PA02/arraylist.hpp
@@ -3,3 +3,29 @@
 //Class: CS1575 Sec A
 //Instructor: Dr. Morales
 //Date: 9-24-19
+
+
+
+
+template <typename T>
+ArrayList<T>::~ArrayList() {
+  delete [] m_data;
+  m_data = NULL;
+  m_size = 0;
+  m_max = 0;
+}
+
+
+template <typename T>
+int ArrayList<T>::size() const {
+  return m_size;
+}
+
+
+template <typename T>
+void ArrayList<T>::clear() {
+  delete [] m_data;
+  m_data = NULL;
+  m_size = 0;
+  m_max = 0;
+}
