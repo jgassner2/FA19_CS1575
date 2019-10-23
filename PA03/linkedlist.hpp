cb66e3e9fe81801dcb9b5d5012c9d4840fe13e6f
Jonathan Gassner <jmgkc7@mst.edu>
2019-10-23 14:16:11 -0500

    Added default constructor.  Unsure if it is correct at the moment. Will update if needed.

--- PA03/linkedlist.hpp
+++ PA03/linkedlist.hpp
@@ -3,3 +3,24 @@
 //Class: CS1575 Sec A
 //Instructor: Dr. Morales
 //Date: 9-24-19
+
+
+template <typename T>
+LinkedList<T>::LinkedList(){
+/*
+  LLNode<T> * tmp;
+  tmp = new LLNode;  //This line won't compile
+  m_size = 0;
+  m_head -> m_data = tmp -> m_data;
+  m_back -> m_data = tmp -> m_data;
+  delete tmp;
+*/
+  //I almost think it is this as the list has no nodes.
+  m_head = m_back = NULL;
+  m_size = 0;
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList(){
+
+}
