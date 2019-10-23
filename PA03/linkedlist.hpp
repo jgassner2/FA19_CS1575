ac7477cfed9cc31ff23160890f10146cb0acfd5f
Jonathan Gassner <jmgkc7@mst.edu>
2019-10-23 15:23:32 -0500

    Udpated default constructor

--- PA03/linkedlist.hpp
+++ PA03/linkedlist.hpp
@@ -7,14 +7,6 @@
 
 template <typename T>
 LinkedList<T>::LinkedList(){
-/*
-  LLNode<T> * tmp;
-  tmp = new LLNode;  //This line won't compile
-  m_size = 0;
-  m_head -> m_data = tmp -> m_data;
-  m_back -> m_data = tmp -> m_data;
-  delete tmp;
-*/
   //I almost think it is this as the list has no nodes.
   m_head = m_back = NULL;
   m_size = 0;
