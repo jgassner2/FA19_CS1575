e79a6b393cf6d50b268560d5fdcaf3b01090625f
Jonathan Gassner <jmgkc7@mst.edu>
2019-10-26 11:08:56 -0500

    Worked on insert_front().  At the moment it will work but there are issues as I cannot delete the tmp pointer.
    Also fixed the inertion operator.  It wasn't moving to the next data object. Outputting
    data currently but valgrind is still reporting reachable blocks.  Might be due to an incomplete
    descrtructor.

--- PA03/linkedlist.hpp
+++ PA03/linkedlist.hpp
@@ -2,7 +2,7 @@
 //SID: 12299764
 //Class: CS1575 Sec A
 //Instructor: Dr. Morales
-//Date: 9-24-19
+//Date: 10-21-19
 
 
 template <typename T>
@@ -42,34 +42,39 @@ const LLNode<T> * LinkedList<T>::getFirstPtr() const {
   }
 }
 
-
-
-
-
-/*
+//Basic Mutator
 template <typename T>
-LinkedList<T>& operator<< (std::ostream& out, const LinkedList<T>& list)
-{
-  LLNode<T> * tmp = list.m_head;
-  out << "[ ";
+void LinkedList<T>::insert_front(const T& x) {
+  LLNode<T> * tmp;
+  tmp = new LLNode<T>;
+  tmp->m_data = x;
+  tmp->m_next = m_head;
+  m_head = tmp;
+  m_size++;
+  //delete tmp; //If this is here, segfault (core dump)
 
-  out << "]";
-  delete tmp;
-  return out;
 }
-*/
+
+
+
 template <typename T>
 std::ostream& operator<< (std::ostream& out, const LinkedList<T>& list)
 {
   //LLNode<T> * tmp = list.m_head;  //m_head is private, need accessor
   //LLNode<T> * tmp = list.getFirstPtr();  //Wont' work unless it's const pointer
   const LLNode<T> * tmp = list.getFirstPtr(); //ASK!!!!
+
+
   out << "[ ";
+
   for (int i=0; i<list.size(); i++){
     out << tmp->m_data << ", "; //Access LLNode actual data
-    tmp->m_next; //move the tmp pointer to point to the next node's member data for the next round
+    //tmp->m_next; //move the tmp pointer to point to the next node's member data for the next round. JUST REPEATS!!!!!
+    tmp = tmp->m_next; //move the tmp pointer to point to the next node's member data for the next round
   }
+
   out << "]";
+
   delete tmp;
   return out;
 }
