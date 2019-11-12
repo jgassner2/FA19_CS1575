7be70bba9c86c26ebaa43e605dc7d92071ac56dc
Jonathan Gassner <jmgkc7@mst.edu>
2019-11-12 13:45:08 -0600

    initial commit of abstractqueue.hpp and popplers.cpp

---
+++ PA04/abstractqueue.hpp
@@ -0,0 +1,7 @@
+//Name: Joanthan Gassner
+//SID: 12299764
+//Class: CS1575 Sec A
+//Instructor: Dr. Morales
+//Date: 11-11-19
+
+#include "abstractqueue.h"
---
+++ PA04/popplers.cpp
@@ -0,0 +1,16 @@
+//Name: Joanthan Gassner
+//SID: 12299764
+//Class: CS1575 Sec A
+//Instructor: Dr. Morales
+//Date: 11-11-19
+
+#include <iostream>
+#include "abstractqueue.h"
+using namespace std;
+
+int main()
+{
+
+  cout << endl << "End Program." << endl;
+  return 0;
+}
