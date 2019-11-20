207c7cd6c7a4fb928c9a76103bf2be0d0d715a25
Jonathan Gassner <jmgkc7@mst.edu>
2019-11-20 16:52:31 -0600

    This is attempt two at the program.  Starting out with everthing in popplers.cpp and will seperate later.  The framework is laid out, compiles, and a derived object can be created.

---
+++ PA04_V2/popplers.cpp
@@ -0,0 +1,77 @@
+//Name: Joanthan Gassner
+//SID: 12299764
+//Class: CS1575 Sec A
+//Instructor: Dr. Morales
+//Date: 11-11-19
+
+
+#include <iostream>
+#include "abstractqueue.h"
+using namespace std;
+
+//For now, placing the queue class here in main program for simplicity.
+//Will move to a seperate file once things are working.
+template <typename T>
+class ArrQueue : public AbstractQueue<T>
+{
+  private:
+    int m_size;
+    int m_max;
+    T * m_data;
+  public:
+  bool isEmpty() const;
+  const T& front() const throw (Oops);
+  const T& back() const throw (Oops);
+  void enqueue(const T& x);
+  void dequeue();
+  void clear();
+  //~ArrQueue();
+
+
+
+};
+
+
+//MAIN
+int main()
+{
+  ArrQueue<int> t;
+
+  if(t.isEmpty())
+    cout << endl <<"Banana Pancake" << endl;
+
+
+cout << endl << "End Program" << endl;
+  return 0;
+}
+
+template <typename T>
+bool ArrQueue<T>::isEmpty() const {
+  //Not finished just making sure it is working
+return true;
+}
+
+template <typename T>
+const T& ArrQueue<T>::front() const throw (Oops) {
+
+}
+
+template <typename T>
+const T& ArrQueue<T>::back() const throw (Oops) {
+
+}
+
+template <typename T>
+void ArrQueue<T>::enqueue(const T& x) {
+
+}
+
+template <typename T>
+void ArrQueue<T>::dequeue() {
+
+}
+
+template <typename T>
+void ArrQueue<T>::clear() {
+
+}
